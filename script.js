1243ba3f926e4a041a4f50c1be762354c942dfc0
Arisha <arishaaa333@gmail.com>
2023-10-10 16:16:35 +0530

    modify

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const plusBtn = document.querySelector(".plus-sign");
-const minusBtn = document.querySelector(".minus-sign");
-const texts = document.querySelectorAll(".text");
-const question = document.querySelector(".question");
+// Select All accordian headers and contents
+const accordianHeaders  = document.querySelectorAll(".accordian-header");
+const accordianContents  = document.querySelectorAll(".accordian-content");
 
-plusBtn.forEach((plusBtn, index) => {
-    plusBtn.addEventListener("click", function () {
-        texts[index].style.display = "block";
-        plusBtn.style.display = "none";
-        minusBtn.style.display = "block";
-})  
+
+// get each accordian header
+accordianHeaders.forEach((header, index) => {
+
+    const toggleButton = header.querySelector(".accordian-toggle");    // get toggle button
+
+    header.addEventListener("click", function(){       // add event listener to header
+
+        accordianContents[index].classList.toggle("active");       // add class to associated header
+        
+        toggleButton.textContent = toggleButton.textContent === "+" ? "-" : "+";       // check for + to - conversions
+        
+        header.classList.toggle("active");     // add class for active header
+    })
 });
-
-
-minusBtn.forEach((minusBtn, index) => {
-    minusBtn.addEventListener("click", function () {
-        texts[index].style.display = "none";
-        minusBtn.style.display = "none";
-        plusBtn.style.display = "block";
-})  
- });
